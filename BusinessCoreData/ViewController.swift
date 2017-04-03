1e3587387f8f7c8edae4f9e0c1925f2f5060f140
Burenkovuser <nb2207@yandex.ru>
2017-04-03 17:41:58 +0600

    Initial Commit

---
+++ BusinessCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusinessCoreData
+//
+//  Created by Vasilii on 03.04.17.
+//  Copyright © 2017 Vasilii Burenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
